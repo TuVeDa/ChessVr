0792cbe3704414f65c5fc70ef506f3f76e226367
Genevieve Conty <genevieve.conty@AV-MBP-7208.local>
2018-01-30 19:28:09 -0800

    king moves diagnoally holmesss what whaaaattt

--- Assets/MoveKing.cs
+++ Assets/MoveKing.cs
@@ -24,5 +24,17 @@ public class MoveKing : MonoBehaviour {
 
         if (Input.GetKeyUp(KeyCode.RightArrow))
             transform.Translate(Vector3.right * moveSpeed);
+
+		if (Input.GetKeyUp(KeyCode.W))
+			transform.Translate((Vector3.forward + Vector3.left) * moveSpeed);
+
+        if (Input.GetKeyUp(KeyCode.S))
+            transform.Translate((Vector3.back + Vector3.right) * moveSpeed);
+
+        if (Input.GetKeyUp(KeyCode.A))
+			transform.Translate((Vector3.left + Vector3.back) * moveSpeed);
+
+        if (Input.GetKeyUp(KeyCode.D))
+			transform.Translate((Vector3.right + Vector3.forward) * moveSpeed);
     }
 }
