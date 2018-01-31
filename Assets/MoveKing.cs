810d192eb397b1b4df6dbeacd684fe894b10fd09
Genevieve Conty <genevieve.conty@AV-MBP-7208.local>
2018-01-30 19:12:31 -0800

    king moves precisely on various spaces using arrows...yaaasssssss

--- Assets/MoveKing.cs
+++ Assets/MoveKing.cs
@@ -5,31 +5,24 @@ using UnityEngine;
 public class MoveKing : MonoBehaviour {
 
     // Use this for initialization
-    private Rigidbody king;
-    private Vector3 kingEndPosition;
-    public float moveSpeed = 10f;
-    public float turnSpeed = 50f;
-    void Start () {
-        king = GetComponent<Rigidbody>();
-        Vector3 kingStartPosition = king.transform.position;
-        Vector3 kingEndPostition = kingStartPosition;
-        kingEndPostition.z += 8.5f;
-        Debug.Log(kingStartPosition);
-        Debug.Log(kingEndPostition);
-    }
+	public float moveSpeed; 
+
+	void Start () {
+		moveSpeed = 9;
+	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKey(KeyCode.UpArrow))
-            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+        if (Input.GetKeyUp(KeyCode.UpArrow))
+			transform.Translate(Vector3.forward * moveSpeed);
 
-        if (Input.GetKey(KeyCode.DownArrow))
-            transform.Translate(Vector3.back * moveSpeed * Time.deltaTime);
+        if (Input.GetKeyUp(KeyCode.DownArrow))
+            transform.Translate(Vector3.back * moveSpeed);
 
-        if (Input.GetKey(KeyCode.LeftArrow))
-            transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+        if (Input.GetKeyUp(KeyCode.LeftArrow))
+            transform.Translate(Vector3.left * moveSpeed);
 
-        if (Input.GetKey(KeyCode.RightArrow))
-            transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+        if (Input.GetKeyUp(KeyCode.RightArrow))
+            transform.Translate(Vector3.right * moveSpeed);
     }
 }
